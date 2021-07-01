d73ca1e94c0a7350b933bdfbd856fffe2c6f274e
Andy <jewiv83667@edmondpt.com>
2021-07-01 16:16:53 +0700

    Day 2 add eslint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('1')
-})
+	            res.send('1gi');
+});
 
 app.listen(3000, () => {
-    console.log('server run at port 3000')
-})
+	console.log('server run at port 3000');
+});
