4b4051ed92016e8c6b80bd762ca9918352d693e8
Andy <jewiv83667@edmondpt.com>
2021-07-12 17:18:27 +0700

    add send

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	            res.send('1gi');
+	            res.send('Bac lai danh cho quan dit lennn');
 });
 
 app.listen(3000, () => {
